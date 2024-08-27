c0ec618f98373d8db64e06145fd811406a13e2bc
emalisuranga <rmeisuranga@gmail.com>
2024-08-27 18:08:38 +0900

    roll back comment

--- src/Page/Payroll/AddSalaryDetails.jsx
+++ src/Page/Payroll/AddSalaryDetails.jsx
@@ -3,7 +3,7 @@ import { Box, Typography } from "@mui/material";
 import EmployeeHeader from "../../Page/Employee/EmployeeHeader";
 import EmployeeSearch from "../Employee/EmployeeSearch";
 import getSalarySections from "../../utils/salarySections";
-// import LoadingAnimation from "../../component/LoadingAnimation";
+import LoadingAnimation from "../../component/LoadingAnimation";
 import styled from "styled-components";
 
 import AddSalaryDetailsSteps from "./AddSalaryDetailsSteps";
@@ -34,7 +34,7 @@ const AddSalary = () => {
     <React.Fragment>
       <EmployeeHeader titleKey="addSalaryForm" />
       <EmployeeSearch onSearch={handleSearch} />
-      {/* 
+      
       {selectedEmployee ? (
         <Box sx={{ p: 3 }}>
           <EmployeeInfoContainer>
@@ -53,24 +53,7 @@ const AddSalary = () => {
         </Box>
       ) : (
         <LoadingAnimation />
-      )} */}
-
-      <Box sx={{ p: 3 }}>
-        <EmployeeInfoContainer>
-          <EmployeeInfo variant="h3" gutterBottom>
-            {`${selectedEmployee.firstName} ${selectedEmployee.lastName}`}
-          </EmployeeInfo>
-          <EmployeeInfo variant="h4" gutterBottom>
-            {selectedEmployee.id}
-          </EmployeeInfo>
-        </EmployeeInfoContainer>
-
-        <AddSalaryDetailsSteps
-          sections={sections}
-          onSubmit={handleSubmit}
-          initialData={selectedEmployee}
-        />
-      </Box>
+      )}
     </React.Fragment>
   );
 };
